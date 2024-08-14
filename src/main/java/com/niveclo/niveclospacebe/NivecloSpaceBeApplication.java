bfa68ca33aa62e5aa49f62c4cc2729818879b1f5
王德荣 <3301758149@qq.com>
2024-08-14 11:19:59 +0800

    Initial test.

---
+++ src/main/java/com/niveclo/niveclospacebe/NivecloSpaceBeApplication.java
@@ -0,0 +1,13 @@
+package com.niveclo.niveclospacebe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NivecloSpaceBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NivecloSpaceBeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/niveclo/niveclospacebe/NivecloSpaceBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.niveclo.niveclospacebe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NivecloSpaceBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
